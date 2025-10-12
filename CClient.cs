0c249c67b9e0aea7432b46a3068cddaff20eea46
farid-ss3945 <abbaslifarid2006@gmail.com>
2025-10-12 22:29:01 +0400

    Add files via upload

---
+++ CClient.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+User user = new User();
+user.name = Console.ReadLine();
+
+TcpClient client = new TcpClient();
+client.Connect(IPAddress.Parse("127.0.0.1"), 7777);
+
+
+
+user.date=DateTime.Now;
+ 
+NetworkStream stream = client.GetStream();
+
+Console.WriteLine("1.AZN\n" +
+                  "2.EUR\n" +
+                  "3.USD");
+
+string from=Console.ReadLine();
+
+Console.WriteLine("1.AZN\n" +
+                  "2.EUR\n" +
+                  "3.USD");
+
+string to = Console.ReadLine();
+
+string amount = Console.ReadLine();
+
+string message = from + to + amount;
+byte[] data = Encoding.UTF8.GetBytes(message);
+stream.Write(data, 0, data.Length);
+    
+byte[] buffer = new byte[1024];
+int bytesRead = stream.Read(buffer, 0, buffer.Length);
+string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+
+
+Console.WriteLine("Server Response: "+response);
+
+user.converted = response;  
+
+string filename = user.name + ".json";
+string json = JsonSerializer.Serialize(user);
+File.WriteAllText(filename, json);
+
+
+client.Close();
+
+
+class User
+{
+    public string name;
+    public DateTime date;
+    public string converted;
+}
---
+++ CServer.cs
@@ -0,0 +1,88 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+TcpListener _listener=new TcpListener(IPAddress.Parse("127.0.0.1"), 7777);
+
+_listener.Start();
+Console.WriteLine("Server started. Waiting for connections...");
+
+while (true)
+{
+    TcpClient client = _listener.AcceptTcpClient();
+    Console.WriteLine("Cliend Connected!");
+    NetworkStream stream = client.GetStream();
+    byte[] buffer = new byte[1024];
+    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+    string message = Encoding.UTF8.GetString(buffer, 0, 1);
+    string message_2 = Encoding.UTF8.GetString(buffer, 1, 1);
+    string message_3 = Encoding.UTF8.GetString(buffer, 2, bytesRead);
+    
+    if (message=="1")
+    {
+        if (message_2=="1")
+        {
+            Console.WriteLine("It's the same🥀🙏");
+        }
+        else if (message_2 == "2")
+        {
+            double temp = Convert.ToDouble((message_3));
+            temp /= 2;
+            message_3 = Convert.ToString(temp);
+        }
+        else if (message_2 == "3")
+        {
+            double temp = Convert.ToDouble((message_3));
+            temp /= 1.7;
+            message_3 = Convert.ToString(temp);
+        }
+    }
+    
+    else if (message=="2")
+    {
+        if (message_2=="2")
+        {
+            Console.WriteLine("It's the same🥀🙏");
+        }
+        else if (message_2 == "1")
+        {
+            double temp = Convert.ToDouble((message_3));
+            temp *= 2;
+            message_3 = Convert.ToString(temp);
+        }
+        else if (message_2 == "3")
+        {
+            double temp = Convert.ToDouble((message_3));
+            temp *= 1.16;
+            message_3 = Convert.ToString(temp);
+        }
+    }
+    
+    else if (message=="3")
+    {
+        if (message_2=="3")
+        {
+            Console.WriteLine("It's the same🥀🙏");
+        }
+        else if (message_2 == "1")
+        {
+            double temp = Convert.ToDouble((message_3));
+            temp *= 1.7;
+            message_3 = Convert.ToString(temp);
+        }
+        else if (message_2 == "2")
+        {
+            double temp = Convert.ToDouble((message_3));
+            temp /= 1.16;
+            message_3 = Convert.ToString(temp);
+        }
+    }
+    
+    string response = message_3;
+    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+    stream.Write(responseBytes, 0, responseBytes.Length);
+
+    client.Close();
+}
+
